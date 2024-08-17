e86987ba8303c59c61a92c5ab6fab99b1d8ffa88
Kapil Nadiyapara <kapil.nadiyapara@gmail.com>
2024-08-17 10:23:54 +0530

    Added logic for Add string and respective test cases

--- src/share/utils.test.ts
+++ src/share/utils.test.ts
@@ -4,4 +4,15 @@ describe("String Calculator - add function", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+  test('should return the number for a single number', () => {
+    expect(add('1')).toBe(1);
+  });
+
+  test('should return the sum for two numbers', () => {
+    expect(add('1,4')).toBe(5);
+  });
+
+  test('should handle new lines as delimiters', () => {
+    expect(add('1\n2,5')).toBe(8);
+  });
 });
--- src/share/utils.ts
+++ src/share/utils.ts
@@ -1,8 +1,21 @@
 export const add = (inputString: string): number => {
-    let result = 0
-    console.log('>>>>>>>>>>>', inputString);
-    if (inputString === '') {
-        return result;
-    }
+  let result = 0;
+  if (inputString === "") {
     return result;
-}
+  }
+  let delimiter = /,|\n/;
+  if (inputString.startsWith("//")) {
+    const parts = inputString.split("\n");
+    delimiter = new RegExp(parts[0].slice(2));
+    inputString = parts[1];
+  }
+
+  const numArray = inputString.split(delimiter).map(Number);
+  const negatives = numArray.filter((n) => n < 0);
+
+  if (negatives.length) {
+    throw new Error(`Negative inputString not allowed: ${negatives.join(",")}`);
+  }
+
+  return numArray.reduce((sum, num) => sum + num, 0);
+};
